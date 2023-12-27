9bb57159b55314e55b8fc86cf06fc3e60875b3ad
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2023-12-27 12:48:30 -0800

    Added animation

--- WebAppVideo/WebAppVideo/wwwroot/js/site.js
+++ WebAppVideo/WebAppVideo/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 450) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
